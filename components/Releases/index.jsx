b468173d6f0e226bbd6168ac49e0cf62d482ac4c
Joep de Jong <j.dejong-16@student.tudelft.nl>
2021-05-12 22:51:35 +0200

    Add album covers

--- components/Releases/index.jsx
+++ components/Releases/index.jsx
@@ -1,14 +1,13 @@
 import Placeholder from '../Placeholder'
+import Albums from './Albums'
 import styles from './Releases.module.scss'
 
 export default function Releases() {
     return (
         <div id="releases" className={styles.container}>
             <div className={styles.content}>
+                <Albums/>
                 <h2>Albums</h2>
-            </div>
-            <div className={styles.content}>
-                <h2>Singles &amp; EP's</h2>
             </div>
         </div>
     )
