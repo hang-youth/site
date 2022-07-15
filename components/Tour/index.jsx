26cad664866ce317073744ba4523df37a959088a
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-07-15 13:32:16 +0200

    Add tour dates

--- components/Tour/index.jsx
+++ components/Tour/index.jsx
@@ -3,6 +3,9 @@ import Show from './Show'
 import styles from './Tour.module.scss'
 
 export default function Tour({ tour }) {
+    // Filter tour to only show dates that are in the future
+    tour = tour.filter(show => new Date(show.date) > new Date())
+    
     return (
         <div id="tour" className={styles.container}>
             {/* <div className={styles.background}/> */}
