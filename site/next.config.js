41d1a64aa3170a433a37bb13c8cea10849e19a1a
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-08-28 00:33:21 +0200

    change dist dir

--- site/next.config.js
+++ site/next.config.js
@@ -10,6 +10,7 @@ const isVendure = provider === '@vercel/commerce-vendure'
 
 module.exports = withCommerceConfig({
   commerce,
+  distDir: 'site/.next',
   i18n: {
     locales: ['nl'],
     defaultLocale: 'nl',
