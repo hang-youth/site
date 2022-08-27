9fe5f09fb609a5c596defc3ddd6b0be4c0b7686e
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-08-27 22:34:31 +0200

    Add Tour and Contact

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -4,8 +4,8 @@ export default function Contact() {
   return (
     <BasePage title="Contact - Hang Youth">
       <h1>Contact</h1>
-      <p><b>Boekingen:</b> Jeps Salfischberger (<a className='link link--inline' href="mailto:j.salfischberger@mojo.nl">j.salfischberger@mojo.nl</a>)<br/>
-      <b>Pers, management, etc:</b> Rick Bakker (<a className='link link--inline' href="mailto:rick@resort.net">rick@resort.net</a>)<br/>
+      <p><b>Boekingen:</b> Jeps Salfischberger op <a className='link link--inline' href="mailto:j.salfischberger@mojo.nl">j.salfischberger@mojo.nl</a><br/>
+      <b>Pers, management, etc:</b> Rick Bakker op <a className='link link--inline' href="mailto:rick@resort.net">rick@resort.net</a><br/>
       <b>Fanmail &amp; opbouwende kritiek:</b> <a className='link link--inline' href="mailto:info@hangyouth.nl">info@hangyouth.nl</a></p>
     </BasePage>
   );
--- pages/tour.jsx
+++ pages/tour.jsx
@@ -1,9 +1,61 @@
 import BasePage from '../components/BasePage';
+import { createClient } from 'contentful';
 
-export default function Tour() {
+import styles from '../styles/Tour.module.scss';
+
+const parseDate = (date) => {
+  const [year, month, day] = date.split('-');
+  const months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
+
+  return `${day} ${months[month - 1]} ‘${year.substring(2)}`;
+}
+
+export default function Tour({tour}) {
   return (
     <BasePage title="Tour - Hang Youth">
       <h1>Tour</h1>
+      <table className={styles.table}>
+        <tbody>
+          {tour.map((item, key) => (
+            <tr key={key}>
+              <td>{parseDate(item.date)}</td>
+              <td>{item.venue}, {item.city}</td>
+              <td><a href={item.ticketSaleUrl} target="_blank" className="link">Koop Kaarten</a></td>
+            </tr>
+          ))}
+          <tr><td>Etc.</td></tr>
+          <tr><td>Etc.</td></tr>
+          <tr><td>Etc.</td></tr>
+        </tbody>
+      </table>
     </BasePage>
   );
+}
+
+export async function getServerSideProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  const data = await client.getEntries({
+    content_type: 'tourDate',
+    order: 'fields.date',
+    'fields.date[gte]': new Date(),
+  })
+
+  const tour = data.items.map(({fields}) => {
+    return {
+      city: fields.city,
+      date: fields.date,
+      venue: fields.venue,
+      ticketSaleUrl: fields.ticketSaleUrl,
+    }
+  })
+
+  return {
+    props: {
+      tour: tour
+    }
+  }
 }
