b34d18489004b6c0aa5df05be97b5899594c55ab
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-08-28 21:20:37 +0200

    Add Statement to Webshop

--- site/pages/webwinkel.jsx
+++ site/pages/webwinkel.jsx
@@ -1,5 +1,6 @@
 import commerce from '@lib/api/commerce'
 import Link from 'next/link'
+import { useState } from 'react';
 
 import BasePage from '../components/BasePage';
 
@@ -8,11 +9,33 @@ import styles from '../styles/Webshop.module.scss';
 const placeholderImg = '/images/product-img-placeholder.svg'
 
 export default function Webshop({products}) {
-  console.log(products)
+  const [showStatement, setShowStatement] = useState(false)
 
   return (
     <BasePage title="Webwinkel - Hang Youth">
       <h1>Webwinkel</h1>
+      <div className={styles.statement}>
+        { showStatement ? (
+          <>
+            <button onClick={() => setShowStatement(false)}>KLIK HIER OM HET STATEMENT WEER IN TE KLAPPEN</button>
+            <h3>1. Prijzen</h3>
+            <p>We willen jullie geen troep aansmeren, dus daarom zijn onze shirts van goede kwaliteit en mooi bedrukt. En omdat we
+            van mooie dingen houden, kiezen we soms ook voor een vette kleur, bedrukte mouwen, rugopdruk, etc. Dat betekent
+            allemaal ook iets voor de inkoopprijs; die ligt wat hoger dan bij een goedkoop Fruit of the Loom-shirt dat je na twee
+            keer wassen kunt weggooien. Daarnaast bieden we bijna al onze muziek gratis aan op Spotify, en daar houden we niet
+            per se veel aan over. Door het kopen van merch kun je ons direct supporten &lt;3 En dan kunnen wij doen alsof we van
+            deze punk bullshit kunnen leven. Tot slot kun je bij ieder item in de winkel zien wat de productiekosten zijn en hoeveel
+            wij er per person (eventueel) aan overhouden. Als je wilt weten hoeveel wij in total verdienen aan de band, kun je <a href="/financieel-verslag" className="link link--inline">hier ook ons financieel verslag downloaden.</a></p>
+            <h3>2. Verzending</h3>
+            <p>We doen dit allemaal zelf en vanuit huis. We proberen ervoor te zorgen dat je bestelling binnen een week thuis is. Kan
+            soms wat seller zijn, kan soms wat langer zijn. Mocht er iets aan de hand zijn (verkeerde maat, pakketje is er nog
+            steeds niet, etc.), mail ons dan op <a href="mailto:info@hangyouth.nl" className="link link--inline">info@hangyouth.nl</a> en vermeld in je e-mail je bestelnummer.</p>
+          </>
+        ) : (
+          <button onClick={() => setShowStatement(true)}>KLIK HIER VOOR EEN STATEMENT OVER DE PRIJZEN (EN VERZENDING) VAN ONZE MERCH</button>
+        )}
+
+      </div>
       <div className={styles.products}>
         {products.map(product => (
           <Link href="/product/[slug]" as={`/product/${product.slug}`} key={product.id}>
