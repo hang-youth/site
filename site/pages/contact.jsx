5f6dcbf14264031971311c881d654ea65f7f7f6b
Joep de Jong <j.dejong-16@student.tudelft.nl>
2022-10-10 18:58:53 +0200

    Add returns page

--- site/pages/contact.jsx
+++ site/pages/contact.jsx
@@ -1,4 +1,5 @@
 import BasePage from '../components/BasePage';
+import Link from 'next/link';
 
 export default function Contact() {
   return (
@@ -7,6 +8,7 @@ export default function Contact() {
       <p><b>Boekingen:</b> Jeps Salfischberger op <a className='link link--inline' href="mailto:j.salfischberger@mojo.nl">j.salfischberger@mojo.nl</a><br/>
       <b>Pers, management, etc:</b> Rick Bakker op <a className='link link--inline' href="mailto:rick@resort.net">rick@resort.net</a><br/>
       <b>Fanmail &amp; opbouwende kritiek:</b> <a className='link link--inline' href="mailto:info@hangyouth.nl">info@hangyouth.nl</a></p>
+      <b><Link href="/returns">Retourneren</Link></b>
     </BasePage>
   );
 }
