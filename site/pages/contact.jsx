12d88a8e6558c673829a5b44ac2abfb837df960b
Joep de Jong <joep@joepdejong.com>
2023-02-12 11:48:49 +0100

    ADd merchandise email

--- site/pages/contact.jsx
+++ site/pages/contact.jsx
@@ -5,24 +5,25 @@ export default function Contact() {
   return (
     <BasePage title="Contact - Hang Youth" description="Contactinformatie voor boekingen, pers, management enzo en fanmail.">
       <h1>Contact</h1>
-      <p><b>Boekingen:</b> Jeps Salfischberger op <a className='link link--inline' href="mailto:j.salfischberger@mojo.nl">j.salfischberger@mojo.nl</a><br/>
-      <b>Pers, management, etc:</b> Rick Bakker op <a className='link link--inline' href="mailto:rick@resort.net">rick@resort.net</a><br/>
-      <b>Fanmail &amp; opbouwende kritiek:</b> <a className='link link--inline' href="mailto:info@hangyouth.nl">info@hangyouth.nl</a></p>
+      <p><b>Boekingen:</b> Jeps Salfischberger op <a className='link link--inline' href="mailto:j.salfischberger@mojo.nl">j.salfischberger@mojo.nl</a><br />
+        <b>Pers, management, etc:</b> Rick Bakker op <a className='link link--inline' href="mailto:rick@resort.net">rick@resort.net</a><br />
+        <b>Fanmail &amp; opbouwende kritiek:</b> <a className='link link--inline' href="mailto:info@hangyouth.nl">info@hangyouth.nl</a><br />
+        <b>Bestellingen &amp; retouren:</b> <a className='link link--inline' href="mailto:merchandise@hangyouth.nl">merchandise@hangyouth.nl</a></p>
 
       <h3>Contactgegevens</h3>
       <p>
-      PUNKBAND<br/>
-      Pieter Vlamingstraat 38<br/>
-      1093 AE Amsterdam
+        PUNKBAND<br />
+        Pieter Vlamingstraat 38<br />
+        1093 AE Amsterdam
       </p>
 
       <p>
-      <a href="mailto:info@hangyouth.nl">info@hangyouth.nl</a><br/>
-      <a href="https://instagram.com/hang.youth">instagram.com/hang.youth</a>
+        <a href="mailto:info@hangyouth.nl">info@hangyouth.nl</a><br />
+        <a href="https://instagram.com/hang.youth">instagram.com/hang.youth</a>
       </p>
       <p>
-      KVK: 82731853<br/>
-      Vestigingsnummer: 0000489448489
+        KVK: 82731853<br />
+        Vestigingsnummer: 0000489448489
       </p>
       <p><b><Link href="/verzend-beleid">Verzend- en Leveringsbeleid</Link></b> en <b><Link href="/algemene-voorwaarden">Algemene Voorwaarden</Link></b></p>
     </BasePage>
--- site/pages/verzend-beleid.jsx
+++ site/pages/verzend-beleid.jsx
@@ -4,8 +4,8 @@ export default function ShippingPolicy() {
   return (
     <BasePage title="Verzend- en Leveringsbeleid - Hang Youth" description="Verzend- en Leveringsbeleid.">
 
-<h1>Verzend- en Leveringsbeleid</h1>
-      <p>We doen dit allemaal zelf en vanuit huis. We proberen ervoor te zorgen dat je bestelling binnen een week thuis is. Kan soms wat seller zijn, kan soms wat langer zijn. Mocht er iets aan de hand zijn (verkeerde maat, pakketje is er nog steeds niet, etc.), mail ons dan op <a href="mailto:info@hangyouth.nl" className="link link--inline">info@hangyouth.nl</a> en vermeld in je e-mail je bestelnummer.</p>
+      <h1>Verzend- en Leveringsbeleid</h1>
+      <p>We doen dit allemaal zelf en vanuit huis. We proberen ervoor te zorgen dat je bestelling binnen een week thuis is. Kan soms wat seller zijn, kan soms wat langer zijn. Mocht er iets aan de hand zijn (verkeerde maat, pakketje is er nog steeds niet, etc.), mail ons dan op <a href="mailto:merchandise@hangyouth.nl" className="link link--inline">merchandise@hangyouth.nl</a> en vermeld in je e-mail je bestelnummer.</p>
 
       <p>Alle zendingen worden verzonden met PostNL.</p>
 
@@ -14,7 +14,7 @@ export default function ShippingPolicy() {
 
       <p>Retourneren kan tot uiterlijk 14 dagen na ontvangst van de bestelling. Het shirt moet ongedragen en ongewassen zijn. De verzendkosten voor het retourneren zijn deels voor eigen rekening, ongeveer 50/50. Zodra wij het shirt retour hebben ontvangen, storten wij het aankoopbedrag terug op je rekening.</p>
 
-      <p>Een retourlabel kan aangevraagd worden door een mail te sturen naar <a className='link link--inline' href="mailto:info@hangyouth.nl">info@hangyouth.nl</a>. Vermeld hierbij je naam, ordernummer en de artikelen die je wilt retourneren. Als je wilt omruilen, vermeld dan ook welke maat je wilt ontvangen. Wij sturen je dan zo snel mogelijk per email een retourlabel toe. Dit kan enkele dagen duren.</p>
+      <p>Een retourlabel kan aangevraagd worden door een mail te sturen naar <a className='link link--inline' href="mailto:merchandise@hangyouth.nl">merchandise@hangyouth.nl</a>. Vermeld hierbij je naam, ordernummer en de artikelen die je wilt retourneren. Als je wilt omruilen, vermeld dan ook welke maat je wilt ontvangen. Wij sturen je dan zo snel mogelijk per email een retourlabel toe. Dit kan enkele dagen duren.</p>
     </BasePage>
   );
 }
